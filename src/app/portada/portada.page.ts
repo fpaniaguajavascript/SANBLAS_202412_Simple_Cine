798ce35866594117a8a7eb63b6b092b5297b933f
fpaniaguajavascript <fernando.paniagua.javascript@gmail.com>
2025-01-23 09:45:02 +0100

    Update portada.page.ts

--- src/app/portada/portada.page.ts
+++ src/app/portada/portada.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonImg} from '@ionic/angular/standalone';
+import { IonContent, IonImg} from '@ionic/angular/standalone';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -9,7 +9,7 @@ import { RouterLink } from '@angular/router';
   templateUrl: './portada.page.html',
   styleUrls: ['./portada.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonImg, RouterLink]
+  imports: [IonContent, CommonModule, FormsModule, IonImg, RouterLink]
 })
 export class PortadaPage implements OnInit {
 
