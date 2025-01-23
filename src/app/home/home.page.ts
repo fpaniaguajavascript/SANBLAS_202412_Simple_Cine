35c8ae11ced32e4eefdb3b01066e4b724a94c2c7
fpaniaguajavascript <fernando.paniagua.javascript@gmail.com>
2025-01-23 09:48:06 +0100

    Cambios en el replace

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { IonList, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonList, IonContent } from '@ionic/angular/standalone';
 import { PeliculasWebService } from '../services/peliculas-web.service';
 import { IMovie } from '../interfaces/imovie';
 import { CommonModule } from '@angular/common';
@@ -9,7 +9,7 @@ import { FichapeliculaComponent } from "../components/fichapelicula/fichapelicul
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonList, IonHeader, IonToolbar, IonTitle, IonContent, CommonModule, FichapeliculaComponent],
+  imports: [IonList, IonContent, CommonModule, FichapeliculaComponent],
 })
 export class HomePage {
   servicioPeliculas = inject(PeliculasWebService);
